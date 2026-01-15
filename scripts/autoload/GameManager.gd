b54ce939dc899510a276f5d17bb6e8d9b58eb734
Tiger <tiger@scerpark.cn>
2026-01-15 20:12:20 +0800

    feat: 实现用户名(局内昵称)的设置

--- scripts/autoload/GameManager.gd
+++ scripts/autoload/GameManager.gd
@@ -2,3 +2,4 @@ extends Node
 
 var data_origin: String = ""
 var ip_begin: String = "192.168."
+var username: String = ""
--- scripts/settings/settings.gd
+++ scripts/settings/settings.gd
@@ -3,10 +3,12 @@ extends Node2D
 func _ready() -> void:
 	$DataSetting/LineEdit.text = GameManager.data_origin
 	$IPBeginSetting/LineEdit.text = GameManager.ip_begin
+	$UsernameSetting/LineEdit.text = GameManager.username
 
 func _on_save_button_pressed() -> void:
 	GameManager.data_origin = $DataSetting/LineEdit.text
 	GameManager.ip_begin = $IPBeginSetting/LineEdit.text
+	GameManager.username = $UsernameSetting/LineEdit.text
 	SceneManager.goto_scene("main_menu")
 
 
