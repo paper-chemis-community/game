c8f27cb4c99b43cbffe81eec39a30bc2be440010
Tiger <tiger@scerpark.cn>
2026-01-21 11:50:43 +0800

    feat: 实现下载检查功能

--- scripts/autoload/DownloadManager.gd
+++ scripts/autoload/DownloadManager.gd
@@ -7,11 +7,16 @@ func download_file(server_path: String, local_path: String) -> void:
 
 func download_from_origin() -> int:
     var origin = GameManager.data_origin
+    var http = HTTPRequest.new()
 
     if origin.substr(0, 4) != "http":
         return 1
     
-    
+    if http.request(origin) != OK:
+        return 2
+
+    if origin[-1] != "/":
+        origin = origin + "/"
 
     return 0
 
