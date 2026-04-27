da88be55675374837550c63e843d8a585dcd871b
Tiger <tiger@scerpark.cn>
2026-04-27 14:07:53 +0800

    feat: 合并MultiGame.gd和GameLoopManager.gd

--- scripts/main_menu/join_game_ui.gd
+++ scripts/main_menu/join_game_ui.gd
@@ -3,6 +3,6 @@ extends Node2D
 
 func _on_join_game_button_pressed() -> void:
 	var ip: String = $JoinGameEdit.text
-	MultiGame.create_client(ip)
+	GameLoopManager.create_client(ip)
 	$".".hide()
 	SceneManager.goto_scene("game/game")
